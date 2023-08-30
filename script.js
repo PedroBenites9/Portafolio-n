24e826b4a66c501d29a698f24365dff4392649fb
Pedro Benites <pedro.benites99@hotmail.com>
2023-08-30 18:56:43 -0300

    Se creo animacion del header

--- script.js
+++ script.js
@@ -5,4 +5,42 @@ const typed = new Typed('.typed', {
     startDelay: 500
 });
 
+// const header = document.querySelector('.header__contenedor');
+// let prevScrollPos = window.scrollY;
 
+// window.addEventListener('scroll', () => {
+//     const currentScrollPos = window.scrollY;
+//     console.log('1-->' + prevScrollPos)
+//     console.log('2-->' + currentScrollPos)
+//     if (prevScrollPos > currentScrollPos) {
+//         // Mostrar el encabezado al hacer scroll hacia arriba
+//         header.classList.remove('hidden');
+//     } else {
+//         // Ocultar el encabezado al hacer scroll hacia abajo
+//         header.classList.add('hidden');
+//     }
+
+//     prevScrollPos = currentScrollPos;
+// });
+
+const header = document.querySelector('.header__contenedor');
+let prevScrollPos = window.screenY;
+let scrolling = false;
+let scrollTimeout;
+
+window.addEventListener('scroll', () => {
+    prevScrollPos = window.scrollY;
+
+    if (!scrolling) {
+        header.classList.add('hidden');
+        scrolling = true;
+    }
+
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+        if (scrolling) {
+            header.classList.remove('hidden');
+            scrolling = false;
+        }
+    }, 200); // Cambia el valor para ajustar el tiempo de espera después del scroll
+});
